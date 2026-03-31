6356f04ebdbb7080b37d58d0538ae814b12281be
𝘼𝙅𝘼𝙔 <ajaycha1232a@gmail.com>
2026-03-31 17:06:38 +0530

    Update auth.js

--- zivvi/backend/routes/auth.js
+++ zivvi/backend/routes/auth.js
@@ -6,7 +6,9 @@ const jwt = require('jsonwebtoken');
 const sendEmail = require('../utils/mailer');
 
 // 🔥 CONFIG (MANUAL SWITCH)
-const FRONTEND_URL = "http://localhost:5000"; // Change to your frontend URL
+const FRONTEND_URL = "Frontend: https://zivvi-tracker.vercel.app/ 
+    
+// Change to your frontend URL
 
 // REGISTER
 router.post('/register', async (req, res) => {
