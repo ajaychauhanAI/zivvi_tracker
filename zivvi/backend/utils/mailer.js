2b75a5bb187f633c5176359ed48927d1d58a00e6
𝘼𝙅𝘼𝙔 <ajaycha1232a@gmail.com>
2026-04-03 17:13:22 +0530

    Update mailer.js

--- zivvi/backend/utils/mailer.js
+++ zivvi/backend/utils/mailer.js
@@ -10,7 +10,8 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // TLS use karega
+    secure: false,
+    family: 4, // 🔥 FORCE IPv4 (IMPORTANT FIX)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
